74bc4c0c34a250f9ff76d9cefaf4f6607127e529
Mai Huy Hoang <huyhoang.mai@airpay.com>
2020-09-06 10:58:33 +0700

    Finish send2kindle

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # send2kindle
 Python script to send ebooks to Kindle devices
+
+# Usage
+``` bash
+git clone https://github.com/hoangmaihuy/send2kindle.git && cd send2kindle
+```
+
+Change ```config_template.json``` to your email address and kindle address. Command to send books to kindle:
+``` bash
+./send2kindle {first_path_to_your_book} {second_path_to_your_book} 
+```
+
+You can use send2kindle as a command line by adding it to alias. Open your ```.bashrc``` or ```.zshrc``` file and add following line:
+``` bash
+alias s2k="python3 {path_to_send2kindle_directory}/send2kindle.py"
+```
+
+Now send books to kindle is as simple as
+``` bash
+s2k ~/Documents/example_book.pdf 
+```
+
+# FAQ
+### How do I know my kindle email?
+Refer [here](https://www.amazon.com/gp/help/customer/display.html?nodeId=G7NECT4B4ZWHQ8WV) for more information.
+
+
+### I can't log in my Gmail. Error: Application-specific password required
+If you turn on 2-step verification, you need to create an App Password for your Gmail to log in. Refer [here](https://support.google.com/mail/?p=InvalidSecondFactor) for more information.
--- send2kindle.py
+++ send2kindle.py
@@ -1,5 +1,77 @@
+#!/usr/bin/python3
+import sys
 import os
+from os.path import basename
+import argparse
+import json
 import smtplib
+from email.mime.multipart import MIMEMultipart 
+from email.mime.application import MIMEApplication
+from email.mime.base import MIMEBase 
+from email import encoders 
+
+def load_config():
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(cwd, 'config.json'), 'r') as f:
+        try:
+            config = json.load(f)
+        except Exception as e:
+            print("Error: Parse config.json failed. Please recheck config template")
+            sys.exit(0)
+    return config
+
+def _get_smtp_server(email):
+    if email.endswith("@gmail.com"):
+        return ('smtp.gmail.com', 587)
+    elif email.endswith("@outlook.com"):
+        return ('smtp-mail.outlook.com', 587)
+    elif email.endswith("@yahoo.com"):
+        return ('smtp-mail.yahoo.com', 587)
+    else:
+        print("Error: Email not supported, only support Gmail, Outlook and Yahoo mail")
+        sys.exit(0)
+
+def send_to_kindle(files):
+    config = load_config()
+    sender_email = config["sender"]["email"]
+    sender_password = config["sender"]["password"]
+    smtp_server = _get_smtp_server(sender_email)
+
+    msg = MIMEMultipart()
+    msg['Subject'] = 'Send to Kindle using send2kindle'
+    msg['From'] = sender_email
+    for file_path in files:
+        file_name = file_path.name
+        with open(file_name, "rb") as f:
+            part = MIMEApplication(f.read())
+        part['Content-Disposition'] = 'attachment; filename="%s"' % basename(file_name)
+        msg.attach(part)
+
+    try:
+        with smtplib.SMTP(*smtp_server) as s:
+            print("Connecting to SMTP Server...")
+            s.ehlo()
+            s.starttls()
+            print("Logging you in...")
+            s.login(sender_email, sender_password)
+            print("Log in successfully!")
+            for device in config["devices"]:
+                receiver_email = device["kindle_email"]
+                device_name = device["name"]
+                opt = input("Send to %s? y/[n] " % device_name)
+                if opt in ['Y', 'y', '']:
+                    msg['To'] = receiver_email
+                    s.sendmail(sender_email, receiver_email, msg.as_string())
+                    print("Send to %s successfully!" % device_name)
+
+    except Exception as e:
+        print("Error: Sending email exception: %s" % e)
 
 if __name__ == '__main__':
-    pass
+    parser =  argparse.ArgumentParser()
+    parser.add_argument(
+        'files', type=argparse.FileType('r'), nargs='+', 
+        help="Files to send in format: doc, docx, mobi, pdf,..."
+    )
+    args = parser.parse_args()
+    send_to_kindle(args.files)
